8f062c7749b327f15d9c60345c064d1f9ed24291
mochibot <amusaur83@gmail.com>
2019-11-12 14:04:17 -0800

    day2 MVP: lru_cache completed

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,9 @@
+import sys, os
+dir_name = os.getcwd()
+sys.path.append(dir_name + '\\doubly_linked_list')   #I had to add this to the path name for the import to work
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +13,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.nodes = 0
+        self.entries = DoublyLinkedList()
+        self.storage = {}
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +25,13 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
-    def get(self, key):
-        pass
+    def get(self, key):     
+        if key in self.storage.keys():
+            node = self.storage[key]
+            self.entries.move_to_front(node)
+            return node.value
+        else:
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +44,17 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if self.nodes == self.limit:                # if limit is reached
+            if key in self.storage:                 # overwrite existing key:value pair
+                self.entries.move_to_front(self.storage[key])
+                self.storage[key].value = value
+            else:                                   # if new key does not exist in cache
+                least = self.entries.tail           # remove least used entry and add new key:value pair
+                self.entries.remove_from_tail() 
+                self.storage = {k:v for k,v in self.storage.items() if v != least}
+                self.entries.add_to_head(value)
+                self.storage[key] = self.entries.head
+        else:                                       
+            self.entries.add_to_head(value)
+            self.nodes += 1
+            self.storage[key] = self.entries.head
