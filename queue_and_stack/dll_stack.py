487283c1196c5e25bbfd0ef2630dd2844adfbcdf
mochibot <amusaur83@gmail.com>
2019-11-11 20:39:59 -0800

    day 1 MVP: queue and stack completed

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,26 @@
+import sys, os
+dir_name = os.getcwd()
+sys.path.append(dir_name + '\\doubly_linked_list')   #I had to add this to the path name for the import to work
 from doubly_linked_list import DoublyLinkedList
-import sys
-sys.path.append('../doubly_linked_list')
+
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.size
